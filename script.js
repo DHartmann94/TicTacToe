8193a5c41b9d323259fc14aa295900bfb8344a53
Daniel Hartmann <daniel.hartmann280@outlook.de>
2023-03-06 19:08:25 +0100

    fonts / animation win lines

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let fields = [];
 let currentShape = 'cross';
 
 function fillShape(id) {
-    if(!fields[id]) {
+    if(!fields[id]) { // check: field occupied
         if(currentShape == 'cross') {
             currentShape = 'circle';
             document.getElementById('player-2').classList.remove('player-inactiv');
@@ -38,31 +38,39 @@ function draw() {
     // Check horizontal lines
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-0').style.transform = 'scaleX(1.0)';
     }
     if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line-1').style.transform = 'scaleX(1.0)';
     }
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line-2').style.transform = 'scaleX(1.0)';
     }
 
     // Check vertical lines
     if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1.0)';
     }
     if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.0)';
     }
     if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.0)';
     }
 
     // Check diagonal lines
     if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0]; 
+        document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.0)';
     }
     if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.0)';
     }
 
     if(!!winner) {
@@ -79,11 +87,32 @@ function checkForWin() {
     ];
   
     for (let [a, b, c] of winningConditions) { // a b und c repräsentieren die 3 Zahlen im array winningConditions
-      if (fields[a] == fields[b] && fields[b] == fields[c] && fields[a]) { // die belegten Felder werden überprüft
-        console.log('GEWONNEN', fields[a]); // wenn fields[a] kreuz oder kreis ist, wird dieser ausgegeben
+      if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a]) { // die belegten Felder werden überprüft
+        console.log('GEWONNEN:', fields[a]); // wenn fields[a] kreuz oder kreis ist, wird dieser ausgegeben
+
+        animationWinningLines(a,b,c); // Animation hinzufügen
         //return fields[a];
       }
     }
-  
     //return null;
+}
+
+function animationWinningLines(a,b,c) { // a b und c repräsentieren die 3 Zahlen im array winningConditions
+    if (a == 0 && b == 1 && c == 2) {
+        document.getElementById('line-0').style.transform = 'scaleX(1.0)';
+    } else if (a == 3 && b == 4 && c == 5) {
+        document.getElementById('line-1').style.transform = 'scaleX(1.0)';
+    } else if (a == 6 && b == 7 && c == 8) {
+        document.getElementById('line-2').style.transform = 'scaleX(1.0)';
+    } else if (a == 0 && b == 3 && c == 6) {
+        document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1.0)';
+    } else if (a == 1 && b == 4 && c == 7) {
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.0)';
+    } else if (a == 2 && b == 5 && c == 8) {
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.0)';
+    } else if (a == 0 && b == 4 && c == 8) {
+        document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.1)';
+    } else if (a == 2 && b == 4 && c == 6) {
+        document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.1)';
+    }
 }
