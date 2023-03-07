32c48bb773123ac56fd3cde3caf293f53572ebe1
Daniel Hartmann <daniel.hartmann280@outlook.de>
2023-03-07 18:56:09 +0100

    responsiv

--- script.js
+++ script.js
@@ -33,53 +33,6 @@ function draw() { // set circle or cross
     }
 }
 
-/*function checkForWin() {
-    let winner;
-
-    // Check horizontal lines
-    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
-        winner = fields[0];
-        document.getElementById('line-0').style.transform = 'scaleX(1.0)';
-    }
-    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
-        winner = fields[3];
-        document.getElementById('line-1').style.transform = 'scaleX(1.0)';
-    }
-    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
-        winner = fields[6];
-        document.getElementById('line-2').style.transform = 'scaleX(1.0)';
-    }
-
-    // Check vertical lines
-    if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
-        winner = fields[0];
-        document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1.0)';
-    }
-    if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
-        winner = fields[1];
-        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.0)';
-    }
-    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
-        winner = fields[2];
-        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.0)';
-    }
-
-    // Check diagonal lines
-    if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
-        winner = fields[0]; 
-        document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.0)';
-    }
-    if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
-        winner = fields[2];
-        document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.0)';
-    }
-
-    if(!!winner) {
-        console.log('GEWONNEN', winner);
-    }
-    
-}*/
-
 function checkForWin() {
     const winningConditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal lines
@@ -121,19 +74,29 @@ function setGameOver() {
     gameOver = true;
     setTimeout(function(){
         document.getElementById('game-over').classList.remove('d-none');
-    }, 2000);
+    }, 100);
 }
 
 function restart() {
     gameOver = false;
-    fields = []; // reset array fields
     document.getElementById('game-over').classList.add('d-none');
 
-    for(let i=0; i <= 7; i++) {
-        document.getElementById(`line-${i}`).classList.add('d-none');
-    }
     for(let i=0; i < fields.length; i++) {
         document.getElementById(`circle-${i}`).classList.add('d-none');
         document.getElementById(`cross-${i}`).classList.add('d-none');
     }
+
+    fields = []; // reset array fields
+    resetAnimationWinLines();
+}
+
+function resetAnimationWinLines(){
+    document.getElementById('line-0').style.transform = 'scaleX(0)';
+    document.getElementById('line-1').style.transform = 'scaleX(0)';
+    document.getElementById('line-2').style.transform = 'scaleX(0)';
+    document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(0)';
+    document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(0)';
+    document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(0)';
+    document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(0)';
+    document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(0)';
 }
